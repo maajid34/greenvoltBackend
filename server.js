b3433815642b1f5fc9d864f4b34b0862c8965b2d
abdi majiid haji <najacade23456@gmail.com>
2026-05-06 14:08:08 +0300

    chaned

--- server.js
+++ server.js
@@ -95,11 +95,25 @@ app.use(notFound);
 app.use(errorHandler);
 
 /* ================= DB + SERVER ================= */
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB Connected");
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection failed:", err.message);
+//     process.exit(1);
+//   });
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(PORT, () => {
+
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
